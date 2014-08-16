c4b8eec74ce9aad408618ab82fe3dd2aa7800756
Kent Fredric <kentfredric@gmail.com>
2014-08-16 18:11:38 +1200

    Build results of dd688ab (on master)

--- lib/Color/Swatch/ASE/Writer.pm
+++ lib/Color/Swatch/ASE/Writer.pm
@@ -5,7 +5,7 @@ use utf8;
 
 package Color::Swatch::ASE::Writer;
 
-our $VERSION = '0.001002';
+our $VERSION = '0.001003';
 
 # ABSTRACT: Low level ASE ( Adobe Swatch Exchange ) file Writer.
 
@@ -270,7 +270,7 @@ Color::Swatch::ASE::Writer - Low level ASE ( Adobe Swatch Exchange ) file Writer
 
 =head1 VERSION
 
-version 0.001002
+version 0.001003
 
 =head1 SYNOPSIS
 
